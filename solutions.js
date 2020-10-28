dcc69dd850576b01eecfb4e015235d532b4734b7
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-28 17:32:14 -0600

    Fizz Buzz answered

--- solutions.js
+++ solutions.js
@@ -15,6 +15,17 @@ for (i = 0; i <= 200; i += 2) {
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+for (i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
 
 ////////////////////////////////
 // Wild Wild Life
