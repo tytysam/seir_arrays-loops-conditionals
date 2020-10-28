cebf66622ad67f774d4b65d01416fcbbc89e1cdd
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-28 17:38:36 -0600

    Wild Wild Life answered

--- solutions.js
+++ solutions.js
@@ -30,6 +30,18 @@ for (i = 1; i < 101; i++) {
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+plantee[2] = 5001;
+wolfy[3] = "Gotham City";
+dart.push("Hawkins");
+wolfy[0] = "Gameboy";
+
+console.log(dart);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
