ea1f9428e98572097df9dbd3360272f4b0355907
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-10-28 17:42:50 -0600

    Yell at the Ninja Turtles answered

--- solutions.js
+++ solutions.js
@@ -1,51 +1,56 @@
-////////////////////////////////
-// Easy Going
-////////////////////////////////
-for (i = 1; i < 21; i++) {
-  console.log(i);
-}
+// ////////////////////////////////
+// // Easy Going
+// ////////////////////////////////
+// for (i = 1; i < 21; i++) {
+//   console.log(i);
+// }
 
-////////////////////////////////
-// Get Even
-////////////////////////////////
-for (i = 0; i <= 200; i += 2) {
-  console.log(i);
-}
+// ////////////////////////////////
+// // Get Even
+// ////////////////////////////////
+// for (i = 0; i <= 200; i += 2) {
+//   console.log(i);
+// }
 
-////////////////////////////////
-// Fizz Buzz
-////////////////////////////////
-for (i = 1; i < 101; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+// ////////////////////////////////
+// // Fizz Buzz
+// ////////////////////////////////
+// for (i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
-////////////////////////////////
-// Wild Wild Life
-////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// ////////////////////////////////
+// // Wild Wild Life
+// ////////////////////////////////
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
-plantee[2] = 5001;
-wolfy[3] = "Gotham City";
-dart.push("Hawkins");
-wolfy[0] = "Gameboy";
+// plantee[2] = 5001;
+// wolfy[3] = "Gotham City";
+// dart.push("Hawkins");
+// wolfy[0] = "Gameboy";
 
-console.log(dart);
+// console.log(dart);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+for (each of ninjaTurtles) {
+  console.log(each.toUpperCase());
+}
 
 ////////////////////////////////
 // Where is Waldo
